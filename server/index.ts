fd89a46c81e1c18263bc206bb463db6b95d7ac60
Dev Jangid <devsthr123@gmail.com>
2025-07-15 00:49:28 +0530

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,20 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production'
+    ? ['https://nilaya-alibaug.com'] // your GoDaddy domain
+    : ['http://localhost:5173', 'http://localhost:5002', 'http://127.0.0.1:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use((req, res, next) => {
   const start = Date.now();
